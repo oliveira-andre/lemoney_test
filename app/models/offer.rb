f0b82fd8448a2af3b88422566dd5655fba6f4185
Andre Oliveira <desmonplays@gmail.com>
2020-09-10 22:46:48 -0300

    feat: before validate set starts_at as current time

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -5,4 +5,12 @@ class Offer < ApplicationRecord
   validates :advertiser_name, :url, :description, :starts_at, presence: true
   validates :advertiser_name, uniqueness: true
   validates :description, length: { maximum: 500 }
+
+  before_validation :set_starts_at
+
+  private
+
+  def set_starts_at
+    self.starts_at = Time.zone.now
+  end
 end
