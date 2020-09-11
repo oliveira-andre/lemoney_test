71d51d2c16568a1c681906bca4093deaee9c341f
Andre Oliveira <desmonplays@gmail.com>
2020-09-10 23:53:45 -0300

    feat: loading new instance of offer on new method

--- app/controllers/admin/offers_controller.rb
+++ app/controllers/admin/offers_controller.rb
@@ -3,14 +3,21 @@
 module Admin
   # resource conroller offers
   class OffersController < ApplicationController
-    before_action :load_offers
+    before_action :load_offers, only: :index
+    before_action :load_offer, except: :index
 
     def index; end
+
+    def new; end
 
     private
 
     def load_offers
       @offers = Offer.all
+    end
+
+    def load_offer
+      @offer = params[:id] ? Offer.find(params[:id]) : Offer.new
     end
   end
 end
