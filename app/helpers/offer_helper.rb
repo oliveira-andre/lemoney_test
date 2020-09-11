374472f27ad0de5b2ba2f6f6e003b0248e76fbe8
Andre Oliveira <desmonplays@gmail.com>
2020-09-11 03:16:43 -0300

    fix: removing unecessary input date helper

--- app/helpers/offer_helper.rb
+++ app/helpers/offer_helper.rb
@@ -5,8 +5,4 @@ module OfferHelper
   def enable_or_disable(offer)
     offer.enabled? ? '[disable]' : '[enable]'
   end
-
-  def input_current_time
-    Time.zone.now.strftime('%Y-%m-%dT%H:%M:%S')
-  end
 end
