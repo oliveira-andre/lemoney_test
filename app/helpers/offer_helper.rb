fd8d590cc028f52a05640327a90190a0d0931bf3
Andre Oliveira <desmonplays@gmail.com>
2020-09-11 00:22:41 -0300

    feat: current time to inputs values

--- app/helpers/offer_helper.rb
+++ app/helpers/offer_helper.rb
@@ -5,4 +5,8 @@ module OfferHelper
   def enable_or_disable(offer)
     offer.enabled? ? '[disable]' : '[enable]'
   end
+
+  def input_current_time
+    Time.zone.now.strftime('%Y-%m-%dT%H:%M:%S')
+  end
 end
