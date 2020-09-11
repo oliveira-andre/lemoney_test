3c0d82786f43559f89b02e7eb66c73b2a2584419
Andre Oliveira <desmonplays@gmail.com>
2020-09-11 15:38:24 -0300

    feat: launch toastr based on flash controller

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,17 +1,31 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import toastr from 'toastr/toastr';
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+document.addEventListener("turbolinks:load", function (e) {
+  let flashtoastr = document.getElementsByClassName('flashtoastr-item');
+  if(!flashtoastr[0]) return;
+
+  let target = document.getElementsByClassName('flashtoastr-item')[0];
+
+    switch (target.dataset.type) {
+      case 'notice':
+        toastr.info(target.textContent);
+        break;
+      case 'alert':
+        toastr.warning(target.textContent);
+        break;
+      case 'error':
+        toastr.error(target.textContent);
+        break;
+      case 'success':
+        toastr.success(target.textContent);
+        break;
+      default:
+        toastr.info(target.textContent);
+    }
+});
+
