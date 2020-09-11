7471f451818a0ac4e8e06aafcc8df816a0262ba9
Andre Oliveira <desmonplays@gmail.com>
2020-09-11 04:33:15 -0300

    fix: removing unecessary before validation

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -4,14 +4,12 @@
 class Offer < ApplicationRecord
   scope :actives, -> { Offer.all.map { |offer| offer if offer.enabled? } }
 
-  validates :advertiser_name, :url, :description, :starts_at, presence: true
+  validates_presence_of :advertiser_name, :url, :description, :starts_at
   validates :advertiser_name, uniqueness: true
   validates :description, length: { maximum: 500 }
 
   validate :valid_url
   validate :ends_at_in_past
-
-  before_validation :set_starts_at
 
   def enabled?
     return Time.zone.now >= starts_at unless ends_at
@@ -34,7 +32,7 @@ class Offer < ApplicationRecord
   private
 
   def valid_url
-    return if url.match?(URL_MATCHES)
+    return if !url || url.match?(URL_MATCHES)
 
     @errors.add(:url, I18n.t('validations.offer.url.invalid'))
   end
@@ -43,9 +41,5 @@ class Offer < ApplicationRecord
     return if !ends_at || starts_at <= ends_at
 
     @errors.add(:ends_at, I18n.t('validations.offer.ends_at.past'))
-  end
-
-  def set_starts_at
-    self.starts_at = Time.zone.now unless starts_at
   end
 end
